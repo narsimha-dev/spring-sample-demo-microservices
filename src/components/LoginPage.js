32aec43c123ad08f3880c6755e0c256e225502ab
Narasimhulu <44318990+narsimha-dev@users.noreply.github.com>
2021-03-25 12:13:31 +0530

    username change

---
+++ src/components/LoginPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Axios from 'axios'
+
+class LoginPage extends React.Component{
+    constructor(props){
+        console.log("constructor is called")
+        super(props);
+        this.state={
+            userName:'Narasimhulu',
+            password:'',
+            apiMEssage:'',
+            userList:[]
+            
+        }
+        this.isActiuvbe=false
+    }
+    componentDidMount(){
+
+        console.log("componentDidMountv is called")
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount is called")
+    }
+    render(){
+
+        console.log("render is called")
+        
+        return(
+<div>
+    <p>
+        THis is LogionPage
+    </p>
+        <h2>Userame: {this.state.userName}</h2>
+        <h2>PAssword: {this.state.password}</h2>
+        <h3>API message: {this.state.apiMEssage}</h3>
+    </div>
+        )
+    }
+}
+export default LoginPage;

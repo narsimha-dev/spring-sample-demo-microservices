8f4f93a0d6fb233d20c64b8ea15f5c4452f9a33e
Narasimhulu <44318990+narsimha-dev@users.noreply.github.com>
2021-03-25 12:10:54 +0530

    user name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './Kiran.css';
+import LoginPage from './components/LoginPage';
+// import LoginPage from '../src/components/LoginPage'
 
 function App() {
   return (
@@ -9,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <LoginPage/>
       </header>
     </div>
   );
